21efe4100f26508ca8ac43e56e744a715f806417
Mark Doughty <email@markdoughty.co.uk>
2015-08-09 09:33:36 +0100

    Last copy of Eousphoros device tree

---
+++ cm.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/klimtlte/full_klimtlte.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_klimtlte
+PRODUCT_DEVICE := klimtlte
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-T705 \
+    PRODUCT_NAME=klimtlte \
+    PRODUCT_DEVICE=klimtlte \
+    TARGET_DEVICE=klimtlte \
